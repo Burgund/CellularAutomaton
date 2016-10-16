1e0ba2eefda569053c54765b222dd3154b42345f
Maciej Burchardt <burgundt3@gmail.com>
2016-10-17 00:07:57 +0200

    Create Cell class and cell array

---
+++ CellularAutomaton/Cell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellularAutomaton
+{
+    class Cell
+    {
+        //hunger variable determines when the cell will die
+        public int hunger = 0;
+        //TODO in future cells will be divided into many species with different attributes
+        public string species = "none";
+        //isAlive boolean check if cell is alive (surprising, isn't it?)
+        public bool isAlive = false;
+    }
+}
