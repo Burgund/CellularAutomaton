cbdbed1f08c26bb30dc0ab736b78ea6dc892ceb3
Maciej Burchardt <burgundt3@gmail.com>
2016-10-22 10:30:18 +0200

    FormatException prevention

--- CellularAutomaton/Program.cs
+++ CellularAutomaton/Program.cs
@@ -18,11 +18,13 @@ namespace CellularAutomaton
             CellEnvironment environment = new CellEnvironment();
             int loop = 0;
 
-            //setting up array size
+            //setting up array size. Try-catch used for prevent FormatException
             Console.WriteLine("Choose the array size. Write an integer between 8 and 80 and push <enter>.");
             Console.WriteLine("If you want default size (10) just push<enter>.\nNote: You may want to set up your console window size.");
             int arraySize = 0;
-            arraySize = Int32.Parse(Console.ReadLine());
+            try { arraySize = Int32.Parse(Console.ReadLine()); }
+            catch (FormatException) { }
+            catch { Console.WriteLine("Ops! Something went wrong!"); }
 
             if (arraySize < 8 || arraySize > 80) arraySize = 10;
 
